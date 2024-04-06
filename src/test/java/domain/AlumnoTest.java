7ee5a2155ee7cf92715188bbc39ed3e2eef75ef5
Jiun Ming Hsu <hsujm219@gmail.com>
2024-04-06 15:23:20 -0300

    fix typo

--- src/test/java/domain/AlumnoTest.java
+++ src/test/java/domain/AlumnoTest.java
@@ -13,21 +13,21 @@ public class AlumnoTest {
    private Materia dds = new Materia(ads, ayed, syo, pdep, inglesI);
 
    @Test
-   public void pudeCursarSiLaMateriaNoTieneCorrelativas() {
+   public void puedeCursarSiLaMateriaNoTieneCorrelativas() {
       Alumno unAlumno = new Alumno(2090260);
       unAlumno.aprobo();
       Assert.assertTrue("El alumno puede cursar algoritmos porque ésta no tiene correlativas", unAlumno.puedeCursar(ayed));
    }
 
    @Test
-   public void pudeCursarSiCumpleLasCorrelativas() {
+   public void puedeCursarSiCumpleLasCorrelativas() {
       Alumno unAlumno = new Alumno(2090260);
       unAlumno.aprobo(ads, ayed, md, syo, pdep, inglesI);
       Assert.assertTrue("El alumno puede cursar diseño porque cumple las correlativas", unAlumno.puedeCursar(dds));
    }
 
    @Test
-   public void noPudeCursarSiNoCumpleLasCorrelativas() {
+   public void noPuedeCursarSiNoCumpleLasCorrelativas() {
       Alumno unAlumno = new Alumno(2090260);
       unAlumno.aprobo(md, syo, pdep, inglesI);
       Assert.assertFalse("El alumno no puede cursar diseño porque le faltan las correlativas ayed y ads", unAlumno.puedeCursar(dds));
